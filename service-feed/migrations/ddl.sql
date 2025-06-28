437594b1674d6bbbf34692a862178734d6664b8a
Shehab Adel <shehabeldinadel@gmail.com>
2025-06-28 23:25:02 +0300

    added ddl for trades

---
+++ service-feed/migrations/ddl.sql
@@ -0,0 +1,13 @@
+CREATE TABLE feed.trades
+(
+    `sym` String,
+    `x` UInt16,
+    `i` String,
+    `z` UInt8,
+    `p` Decimal64(11),
+    `s` UInt64,
+    `t` UInt64,
+    `q` UInt64,
+    `c` Array(UInt8)
+)
+ORDER BY (sym, t - (t % 60000))
